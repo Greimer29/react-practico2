d86a665133fd616573e3effdd8e0fbd679843f27
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-29 17:30:42 -0700

    Add the component TodoForm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/Todo.form';
+import {todos} from './todos.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos,
+      titulo:"My Tasks"
+    }
+  }
+  render(){
+    const todos = this.state.todos.map((todo,i)=>{
+      return(
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.titulo}</h3>
+              <span> 
+                {todo.prioridad}
+              </span>
+            </div>
+            <div className="card-body">
+              <p> {todo.descripcion} </p>
+              <p><mark>{todo.responsable}</mark></p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+    return(
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="/"className="text-white">
+              {this.state.titulo}
+              <span className="badge badge-light">
+                {this.state.todos.length}
+              </span>
+          </a>
+        </nav>
+            <TodoForm/>
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
