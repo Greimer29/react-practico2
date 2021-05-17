6ec5565341f362676de08b8fd00fbf0302f44d4c
GreimerKmon <greimerabelguzman29@gmail.com>
2021-05-17 14:27:14 -0400

    When an input changes, the value will be placed on the estate of this component

--- src/components/Todo.form.js
+++ src/components/Todo.form.js
@@ -1,63 +1,76 @@
 import React,{Component} from 'react';
 
-class TodoForm extends Component{
-    constructor(){
-        super();
-        this.state={
-            titulo:'',
-            responsable:'',
-            descripcion:'',
-            prioridad:'low'
-        };
-    }
-    handleInput(e){
-        console.log(e.target.name)
-    }
-    render(){
-        return(
-            <div className="card">
-                <form className="card-body">
-                    <div className="form-group">
-                        <input 
-                            type="text" 
-                            name="titulo" 
-                            className="form-control" 
-                            placeholder="Titulo"
-                            onChange={this.handleInput}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input 
-                            type="text" 
-                            name="responsable" 
-                            className="form-control" 
-                            placeholder="Responsable"
-                            onChange={this.handleInput}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input 
-                            type="text" 
-                            name="descripción" 
-                            className="form-control" 
-                            placeholder="Descripción"
-                            onChange={this.handleInput}
-                        />
-                    </div>
-                    <select 
-                        name="prioridad" 
-                        className="form-control"
-                        onChange={this.handleInput}
-                    >
-                        <option>low</option>
-                        <option>medium</option>
-                        <option>high</option>
-                    </select>
-                    <button type="submit" className="btn btn-primary">Save</button>
-                </form>
-            </div>
-        )
-    }
+class TodoForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      titulo: "",
+      responsable: "",
+      descripcion: "",
+      prioridad: "low",
+    };
+    this.handleInput = this.handleInput.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleInput(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+    console.log(e.target.name, e.target.value);
+  }
+
+  // handleSubmit(e){
+  //     e.preventDefault();
+
+  // }
+  render() {
+    return (
+      <div className="card">
+        <form className="card-body" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              name="titulo"
+              className="form-control"
+              placeholder="Titulo"
+              onChange={this.handleInput}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="responsable"
+              className="form-control"
+              placeholder="Responsable"
+              onChange={this.handleInput}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="descripción"
+              className="form-control"
+              placeholder="Descripción"
+              onChange={this.handleInput}
+            />
+          </div>
+          <select
+            name="prioridad"
+            className="form-control"
+            onChange={this.handleInput}
+          >
+            <option>low</option>
+            <option>medium</option>
+            <option>high</option>
+          </select>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default TodoForm;
